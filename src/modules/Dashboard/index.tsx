8bbdb79412df9d3b18094ba031e6a35fada5e0f6
newset <luojieyy@gmail.com>
2021-12-19 23:06:28 +0800

    feat: 添加模拟数据

--- src/modules/Dashboard/index.tsx
+++ src/modules/Dashboard/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Line from "@/components/Line";
+import Line, { DataType } from "@/components/Line";
 import transform from "@/components/Line/transform";
 import data from "./data.json";
-import { makeCardData } from "./helper";
+import { startOfWeek, addDays, format } from "date-fns"
 import works from "./works.json";
 import Card, { CardProgress } from "@/components/Card";
 
@@ -14,11 +14,39 @@ const lossRatio = (
 const DashBoard = () => {
   const list = works.data.matterInfo.list;
   const total:number = list.reduce((pre, cur) => pre + (+cur.cost), 0);
+  const today = new Date();
+  const start = startOfWeek(today);
+  const dept ="001-00A";
+
+  const rand = () => Array(list.length).fill(1).map(()=> Math.random() * 100);
+
+  const chart_data = [
+    start,
+    addDays(start, 1),
+    addDays(start, 2),
+    addDays(start, 3),
+    addDays(start, 4),
+  ].reduce((pre, d) => {
+    const data = rand();
+    const day_total = data.reduce((a, b) => a + b, 0)
+    pre.push(...list.map((item, index)  => {
+      const value = data[index];
+      return {
+        value: +value.toFixed(1),
+        dept, 
+        date: format(d, 'yyyy-MM-dd'),
+        type: item.title,
+        percent: (100 * value /day_total).toFixed(2)
+      }
+    }))
+    console.log('test: ', pre);
+    return pre;
+  }, [] as DataType[]);
 
   return (
     <>
       <Card className="dashboard-top-chart">
-        <Line data={transform(data.data.dayData as any)} dept={"001-00A"} />
+        <Line data={chart_data} dept={dept} />
       </Card>
       <div className="dashboard-detail-cards">
         {
